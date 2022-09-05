6fbcb6016025511874418423d3c92f7b4f54c0fb
fatchurrosii <watashifrdesu@yahoo.com>
2022-09-05 22:10:36 +0700

    first Initialization

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
+  plugins: [
+    require("flowbite/plugin")
+  ],
+  theme: {
+    extend: {
+    },
+    colors:{
+      'primary' : '#8155FF',
+      'dark'  : '#4C4C54',
+      'secondary' : '#F8F5FF',
+      'footer-dark': '#061E30',
+    }, 
+    container:{
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    }
+  },
+};
